02b743c7e5d98bc51a3104b84f8a77747dcbc793
AbuHasanRifat <showmiksopno@gmail.com>
2026-02-21 17:31:48 +0600

    Floyds-triangle program

---
+++ FloydsTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        Scanner flo = new Scanner(System.in);
+
+        System.out.print("row number: ");
+        int rows = flo.nextInt();
+
+        int num = 1;
+
+        for (int i = 1; i <= rows; i++) { 
+            for (int j = 1; j <= i; j++) { 
+            System.out.print(num + " ");
+                num++;
+            }
+             System.out.println();
+        }
+    }
+}
